700f739d025e33f2fe5de24f33848878723beb07
Shivam <choudharyshivam0508@gmail.com>
2026-05-23 20:40:04 +0530

    feat: initialize express app with user registration route and middleware configuration

--- server/src/app.js
+++ server/src/app.js
@@ -10,6 +10,12 @@ app.use(express.json({limit : '16kb'}));
 app.use(express.urlencoded({ extended : true , limit : '16kb'}));
 app.use("/uploads" , express.static("uploads"));
 
-app.use(cookieParser())
+app.use(cookieParser());
+
+
+// Routes Import // 
+import userRoute from './route/user.route.js';
+
+app.use("/api/v1/users" , userRoute);
 
 export default app;
