2ac04da3d56c5d92746d4e69886eb79eb708bf86
Shivam <choudharyshivam0508@gmail.com>
2026-06-08 12:50:13 +0530

    feat: implement verifyJWT middleware to authenticate users via access tokens

---
+++ server/src/middleware/auth.middleware.js
@@ -0,0 +1,44 @@
+import jwt from "jsonwebtoken";
+import { User } from "../model/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      throw new ApiError(400, "Unauthorized request: Token missing...");
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    if (!decodedToken || !decodedToken._id) {
+      throw new ApiError(400, "Invalid token. Please log in again.");
+    }
+
+    const user = await User.findById(decodedToken._id).select(
+      " -password -refreshToken",
+    );
+    if (!user) {
+      throw new ApiError(400, "Invalid access token: user not found...");
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    if (error instanceof ApiError) {
+      throw error;
+    }
+
+    if (error.name === "TokenExpiredError") {
+      throw new ApiError(401, "Access token expired. Please refresh token.");
+    }
+    if (error.name === "JsonWebTokenError") {
+      throw new ApiError(401, "Invalid token. Please log in again.");
+    }
+
+    throw new ApiError(401, "Unauthorized. Please log in.");
+  }
+});
