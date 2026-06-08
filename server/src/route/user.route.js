ee3d1692a75b9a44b6ac403a00b9b7b9de89fc5a
Shivam <choudharyshivam0508@gmail.com>
2026-06-08 16:19:59 +0530

    feat: add user authentication routes for registration, login, logout, and token refresh

--- server/src/route/user.route.js
+++ server/src/route/user.route.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { registerUser , loginUser } from '../controller/user.controller.js';
+import { registerUser , loginUser , logoutUser , currentUser , refreshAccessToken } from '../controller/user.controller.js';
 import { verifyJWT } from '../middleware/auth.middleware.js'
 
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/refresh-token").post(refreshAccessToken)
+
+//? Auth routes;
+
+router.route("/logout").post(verifyJWT , logoutUser);
+router.route("/current-user").post(verifyJWT , currentUser);
 
 export default router
